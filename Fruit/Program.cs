f2e87fbc2fe155c7b5ea8f329eb3da38c1d1fe64
Lauren Lempe <laurenlo02@gmail.com>
2019-03-20 13:56:44 -0400

    Commit

---
+++ Fruit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit
+{
+    class Program
+    {
+        public enum Fruit
+        {
+            Apple,
+            Pear,
+            Strawberry,
+            Blueberry,
+            Banana,
+            Pineapple
+        }
+        
+        static void Main(string[] args)
+        {
+            Fruit[] fruits = new Fruit[3];
+            string userResponse;
+
+
+            for (int index = 0; index < 3; index++)
+            {
+                Console.Write($"Enter Fruit #{index + 1}:");
+                userResponse = Console.ReadLine();
+                Enum.TryParse(userResponse, out fruits[index]);
+            }
+
+            Console.WriteLine();
+
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
